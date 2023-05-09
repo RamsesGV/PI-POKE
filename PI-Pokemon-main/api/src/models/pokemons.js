3efe6fa9d67ec1f433d2f29dcb2ba255df6e3b94
Ramses Garay <senkuStone15@gmail.com>
2023-05-09 10:31:22 -0600

    DB done.

---
+++ PI-Pokemon-main/api/src/models/pokemons.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => { 
+    sequelize.define('Pokemons', { 
+        ID: { 
+            allowNul: false, 
+            primaryKey: true,
+            type:DataTypes.INTEGER
+        },
+        Nombre: { 
+            type:DataTypes.STRING,
+            unique:true
+        },
+        Imagen:{ 
+            isUrl: true,
+            type:DataTypes.STRING
+        },
+        Vida: { 
+            type: DataTypes.INTEGER,
+            
+        },
+        Ataque: { 
+            type: DataTypes.INTEGER,
+        
+        },
+        Defensa: { 
+            type: DataTypes.INTEGER,
+            
+        },
+        Velocidad: { 
+            type: DataTypes.INTEGER,
+            
+        },
+        Altura: { 
+            type: DataTypes.FLOAT
+        },
+        Peso: { 
+            type: DataTypes.FLOAT
+        }
+    },{timestamps:false})
+}
